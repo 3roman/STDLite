8b851afe528a8cc51751a7d53b0c6b2c7ce091a7
ryecatcher <ryecatcher1951@163.com>
2016-01-23 23:34:12 +0800

    项目被创建

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace SEG_SD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.lstMain = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(477, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.TabStop = false;
+            this.btnSearch.Text = "检  索";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(53, 6);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(418, 21);
+            this.txtKeyword.TabIndex = 1;
+            this.txtKeyword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtKeyword_KeyPress);
+            // 
+            // lstMain
+            // 
+            this.lstMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lstMain.FullRowSelect = true;
+            this.lstMain.GridLines = true;
+            this.lstMain.Location = new System.Drawing.Point(6, 32);
+            this.lstMain.MultiSelect = false;
+            this.lstMain.Name = "lstMain";
+            this.lstMain.Size = new System.Drawing.Size(546, 320);
+            this.lstMain.TabIndex = 3;
+            this.lstMain.TabStop = false;
+            this.lstMain.UseCompatibleStateImageBehavior = false;
+            this.lstMain.View = System.Windows.Forms.View.Details;
+            this.lstMain.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstMain_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "序号";
+            this.columnHeader1.Width = 43;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "标准号";
+            this.columnHeader2.Width = 159;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "标准名";
+            this.columnHeader3.Width = 337;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "下载地址";
+            this.columnHeader4.Width = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "关键字";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 354);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstMain);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.btnSearch);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.ListView lstMain;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Windows.Forms;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+
+namespace SEG_SD
+{
+    public partial class Form1 : Form
+    {
+        Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            var dllName = args.Name.Contains(",") ? args.Name.Substring(0, args.Name.IndexOf(',')) : args.Name.Replace(".dll", "");
+            dllName = dllName.Replace(".", "_");
+            if (dllName.EndsWith("_resources")) return null;
+            var rsc = new ResourceManager(GetType().Namespace + ".Properties.Resources",
+                Assembly.GetExecutingAssembly());
+            var buffer = (byte[])rsc.GetObject(dllName);
+            return Assembly.Load(buffer);
+        }
+
+        public void SetFormTitle()
+        {
+            const string title = "{0}  V{1}   c0de by hangch";
+            Text = string.Format(title, Application.ProductName, Application.ProductVersion);
+        }
+
+        public string RetrieveHtml(string url, string data = "", bool get = true)
+        {
+            var wc = new WebClient();
+            wc.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            var postData = Encoding.UTF8.GetBytes(data);
+            byte[] datas = {0};
+            try
+            {
+                datas = get ? wc.DownloadData(url) : wc.UploadData(url, "POST", postData);
+            }
+            catch (Exception ex)
+            {
+                SetFormTitle();
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+
+            return Encoding.UTF8.GetString(datas);
+        }
+
+        private static void DownloadStandard(string filePath, string url)
+        {
+            var wc = new WebClient();
+            var datas = wc.DownloadData(url);
+            var fs = new FileStream(filePath, FileMode.Create);
+            fs.Write(datas, 0, datas.Length);
+            fs.Close();
+
+        }
+
+        public void GetStandard(string url, ref List<object> items)
+        {
+            var postData = @"__EVENTTARGET=stdlist%24lbtn_refresh_1&__VIEWSTATE=%2FwEPDwULLTE2NjMyMjA0OTYPZBYCAgEPZBYEZg8PZBYCHgVzdHlsZQUkbWFyZ2luOjBweDtwYWRkaW5nOjBweDtkaXNwbGF5Om5vbmU7ZAIBDw9kFgIfAAUkZGlzcGxheTpub25lO21hcmdpbjowcHg7cGFkZGluZzowcHg7ZBgBBR5fX0NvbnRyb2xzUmVxdWlyZVBvc3RCYWNrS2V5X18WCwUdc3RkbGlzdCRTb3J0RmllbGRfUGVyZm9ybWRhdGUFHXN0ZGxpc3QkU29ydEZpZWxkX1B1Ymxpc2hkYXRlBR1zdGRsaXN0JFNvcnRGaWVsZF9QdWJsaXNoZGF0ZQUdc3RkbGlzdCRTb3J0RmllbGRfRXhwaXJlZGRhdGUFHXN0ZGxpc3QkU29ydEZpZWxkX0V4cGlyZWRkYXRlBRxzdGRsaXN0JFNvcnRGaWVsZF9QZXJtaXRkZXB0BRxzdGRsaXN0JFNvcnRGaWVsZF9QZXJtaXRkZXB0BRlzdGRsaXN0JFNvcnRGaWVsZF9TdGRjb2RlBRlzdGRsaXN0JFNvcnRGaWVsZF9TdGRjb2RlBRtzdGRsaXN0JFNvZnRGaWVsZF9WaWV3Q291bnQFG3N0ZGxpc3QkU29mdEZpZWxkX1ZpZXdDb3VudO8M73tN9IyZEmIzbo3Rt9RG6tux&__EVENTVALIDATION=%2FwEWKwLO8JzAAwKHpeXgAQL7pL24BAKLtoHNCAKH9772BgKR9%2FPABALA9NmOBwLgoYrZCQLA9N2OBwK9s4GIDgLI%2BY3QAgKX%2BOKqAgLh%2B4PaDQKdsbKgDgKi%2F46dBALzwuvZBgLJ2OjZBgKKy%2BGqDQLqycYZAv7lkOIDAurJyjkC%2FuWUggQC6sme8AEC%2FuW4uQUC6smikAIC%2FuW82QUC6smWsAEC%2FuWg%2BQQC6sma0AEC%2FuWkmQUC6smu6QIC%2FuWIsgYC6smyiQMC%2FuWM0gYC6smmsAIC%2FuWw%2BQUC6smq0AIC%2FuW0mQYCmPePxAoCwPThjgcC96GGwgYCwPTFjgcC5trLyAEB9COtPt6LDYSCzyGfSC34U4IzoA%3D%3D&stdlist%24txt_page_count_top=1000000";
+            var html = RetrieveHtml(url, postData, false);
+            
+            var doc = new HtmlDocument();
+            doc.LoadHtml(html);
+            const string xPath = "//a[@href]";
+            var nodes = doc.DocumentNode.SelectNodes(xPath);
+            if (null == nodes) return;
+
+            foreach (var node in nodes)
+            {
+                if (!node.InnerText.Contains("---")) continue;
+                var innerText = node.InnerText.Replace("---", "^").Split('^');
+                items.Add(new
+                {
+                    number = innerText[1].Replace("/", ""),
+                    name = innerText[0],
+                    link = "http://10.113.1.69/std/showEBook.aspx?fileid=" + node.Attributes["href"].Value.Replace("../std/stdmgr.aspx?fileid=", "")
+                });
+            }
+
+            items = items.Distinct().ToList();
+        }
+
+        private void AddListItems(IEnumerable<object> items )
+        {
+            lstMain.BeginUpdate();
+            lstMain.Items.Clear();
+            
+            foreach (var item in items)
+            {
+                // 通过反射获取匿名对象属性
+                var lvi = new ListViewItem(lstMain.Items.Count.ToString());
+                lvi.SubItems.Add(item.GetType().GetProperty("number").GetValue(item, null).ToString());
+                lvi.SubItems.Add(item.GetType().GetProperty("name").GetValue(item, null).ToString());
+                lvi.SubItems.Add(item.GetType().GetProperty("link").GetValue(item, null).ToString());
+                lstMain.Items.Add(lvi);
+            }
+            lstMain.EndUpdate();
+
+        }
+       
+        public Form1()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            InitializeComponent();
+            SetFormTitle();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            var items = new List<object>();
+
+            Text = "努力检索中...马上就来";
+            var url = string.Format("http://10.113.1.69/std/stdsearch.aspx?key={0}&idx=0&pcnt=10000",
+                txtKeyword.Text);
+            GetStandard(url, ref items);
+            url = string.Format("http://10.113.1.69/std/stdsearch.aspx?key={0}&idx=1&pcnt=10000",
+                txtKeyword.Text);
+            GetStandard(url, ref items);
+            AddListItems(items);
+
+            SetFormTitle();
+
+        }
+
+        private void lstMain_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Cursor = Cursors.WaitCursor;
+            Text = "玩命下载中...稍安勿躁(*@ο@*)";
+            //var filePath = string.Format("{0}\\{1} {2}.pdf",
+            //    Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+            //    lstMain.SelectedItems[0].SubItems[1].Text,
+            //    lstMain.SelectedItems[0].SubItems[2].Text);
+            var filePath = string.Format("{0} {1}.pdf",
+                lstMain.SelectedItems[0].SubItems[1].Text,
+                lstMain.SelectedItems[0].SubItems[2].Text);
+            var uri = lstMain.SelectedItems[0].SubItems[3].Text;
+            DownloadStandard(filePath, uri);
+            Cursor = Cursors.Default;
+            Process.Start(filePath);
+            SetFormTitle();
+
+        }
+
+        private void txtKeyword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 27)
+                txtKeyword.Clear();
+        }
+
+    }
+}
